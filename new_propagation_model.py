de4215382d1dc95238babf85116424cef675244c
Sandra Sabrina Borges <sandrasabrinaborges@outlook.com>
2025-07-11 17:09:22 -0300

    Update new_propagation_model.py

--- new_propagation_model.py
+++ new_propagation_model.py
@@ -21,7 +21,7 @@ def topology():
                        failMode='standalone', position='10,10,0')
 
     info("*** Configuring Propagation Model\n")
-    net.setPropagationModel(model="logNormalShadowing", exp=10, variance=10)
+    net.setPropagationModel(model="logNormalShadowing", exp=10, variance=2)
 
     net.plotGraph(max_x=100, max_y=100)
 
