566e86d7a3098e32de27372370f6143762d10d0f
Sandra Sabrina Borges <sandrasabrinaborges@outlook.com>
2025-11-26 09:35:40 -0300

    Delete NegocioException.java

--- NegocioException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-// Exceção customizada para regras de negócio (checked exception)
-public class NegocioException extends Exception {
-    public NegocioException(String message) {
-        super(message);
-    }
-}
